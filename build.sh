c37b4d931df6f4f9a17bfcd6c6ec2aed3de31f1f
Michal vel m@lbit <michal@arqma.com>
2018-11-22 00:40:16 +0000

    gnumake -> make

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@
 ICU_ROOT=$(pwd)
 
 ./configure_i386.sh
-mkdir -p build-i386 && cd build-i386 && gnumake
+mkdir -p build-i386 && cd build-i386 && make
 
 cd $ICU_ROOT
 
 ./configure_x86_64.sh
-mkdir -p build-x86_64 && cd build-x86_64 && gnumake
+mkdir -p build-x86_64 && cd build-x86_64 && make
 
 cd $ICU_ROOT
 
 ./configure_armv7.sh
-mkdir -p build-armv7 && cd build-armv7 && gnumake
+mkdir -p build-armv7 && cd build-armv7 && make
 
 cd $ICU_ROOT
 
 ./configure_armv7s.sh
-mkdir -p build-armv7s && cd build-armv7s && gnumake
+mkdir -p build-armv7s && cd build-armv7s && make
 
 cd $ICU_ROOT
 
